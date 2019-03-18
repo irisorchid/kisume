813434606ba8ed35586c7cd00a8287e55ee11c2f
irisorchid <edgeoftomorrows@gmail.com>
2019-03-18 02:16:23 -0700

    test git2

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import os
 import discord
+from discord.ext import commands
 
 from dotenv import load_dotenv
 
 #dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-
 load_dotenv(verbose=True)
 
-print(os.getenv("test"))
+discord_bot_token = os.getenv('discord_bot_token')
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Hello World')
+    
+client.run(discord_bot_token)
